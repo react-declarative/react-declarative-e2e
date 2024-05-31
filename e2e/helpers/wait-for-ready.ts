bfb71cae90c630106f7a269fcc767af31fabbfe0
Petr Tripolsky <tripolskypetr@gmail.com>
2024-05-31 22:15:29 +0500

    patch

--- e2e/helpers/wait-for-ready.ts
+++ e2e/helpers/wait-for-ready.ts
@@ -10,4 +10,5 @@ export const waitForReady = async (page: Page) => {
   await waitForIdle(page);
   await page.goto("http://localhost:3000/");
   await waitForIdle(page);
+  await page.waitForTimeout(1000);
 };
